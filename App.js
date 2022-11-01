1fb75b986b36afa68d4b1dd8a055a96404e336ba
Jack <Jackferson15@gmail.com>
2022-11-01 11:19:07 -0300

    package changes

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ContextState from "./context/ContextState";
 import MainPage from "./components/MainPage";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <ContextState>
-      
       <MainPage />
     </ContextState>
   );
